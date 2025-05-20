24dac6eaab6594071ed2951ba5263881aff53124
x1hntr <x1dxniel@gmail.com>
2025-05-20 00:30:44 -0500

    logos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import logo from '../assets/rdp-logo.png';
 import "../styles/header.css";
 
 const Header = () => {
   return (
     <header className="header">
-    <img src="\src\assets\rdp-logo.png" alt="Relatos de Papel" className="header__logo-img" />
+    <img src={logo} alt="Relatos de Papel" className="header__logo-img" />
       <nav className="header__nav">
         <Link className="header__link" to="/">Landing</Link>
         <Link className="header__link" to="/home">Home</Link>
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import logo from '../assets/rdp-logo.png';
 import "../styles/landing.css";
 
 const Landing = () => {
@@ -15,7 +16,7 @@ const Landing = () => {
   return (
     <section className="landing">
       <h1 className="landing__titulo">Bienvenido</h1>
-      <img src="\src\assets\rdp-logo.png" alt="Relatos de Papel" className="landing__logo-img" />
+      <img src={logo} alt="Relatos de Papel" className="landing__logo-img" />
       <p className="landing__descripcion">Encuentra tus libros favoritos en nuestra tienda.</p>
       <button className="landing__boton" onClick={() => navigate("/home")}>
         Ir a la tienda
